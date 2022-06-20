18eccc77aee15c24273f484fe1e1e63b2dada563
Mini Bhati <minibhati93@gmail.com>
2022-06-20 21:47:43 +0530

    jest & atl setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,29 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { render, screen } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  async function setup() {
+    const { fixture } = await render(AppComponent);
+    return {
+      fixture,
+      component: fixture.componentInstance,
+    };
+  }
+
+  it('should create the app', async () => {
+    const { component } = await setup();
+    expect(component).toBeTruthy();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it(`should have as title 'angular-jest-demo'`, async () => {
+    const { component } = await setup();
+    expect(component.title).toBe('angular-jest-demo');
   });
 
-  it(`should have as title 'angular-jest-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-jest-demo');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-jest-demo app is running!');
+  it('should render title', async () => {
+    await setup();
+    expect(screen.getByTestId('app-title').textContent).toBe(
+      'angular-jest-demo app is running!'
+    );
   });
 });
